bb4ba4d2f55463fb4660d6eb5c1290d2a2c5e277
Igor Konovalov <47081014+ColaChanel@users.noreply.github.com>
2024-04-04 10:57:13 +0300

    face_mesh_change

--- online/fresh_view/Demos/face_mesh_app.py
+++ online/fresh_view/Demos/face_mesh_app.py
@@ -1,6 +1,3 @@
-#Modified by Augmented Startups 2021
-#Face Landmark User Interface with StreamLit
-#Watch Computer Vision Tutorials at www.augmentedstartups.info/YouTube
 import streamlit as st
 import mediapipe as mp
 import cv2
@@ -12,16 +9,18 @@ from PIL import Image
 mp_drawing = mp.solutions.drawing_utils
 mp_face_mesh = mp.solutions.face_mesh
 
-DEMO_VIDEO = 'demo.mp4'
-DEMO_IMAGE = 'demo.jpg'
+DEMO_VIDEO = 'fresh_view\Demos\demo.mp4'
+DEMO_IMAGE = 'fresh_view\Demos\demo.jpg'
 
-st.title('Face Mesh Application using MediaPipe')
+st.title('Нанесение лицевой сетки')
 
 st.markdown(
     """
     <style>
-    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
+    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
         width: 350px;
+        margin-left: -350px;
+
     }
     [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
         width: 350px;
@@ -32,8 +31,8 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-st.sidebar.title('Face Mesh Application using MediaPipe')
-st.sidebar.subheader('Parameters')
+st.sidebar.title('Нанесение лицевой сетки')
+st.sidebar.subheader('Параметры')
 
 @st.cache_data()
 def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
@@ -67,10 +66,51 @@ def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
     # return the resized image
     return resized
 
-app_mode = st.sidebar.selectbox('Choose the App mode',
-['About App','Run on Image','Run on Video']
+app_mode = st.sidebar.selectbox('Выберите вид взаимодействия',
+['О приложении','Запустить по видео']
+# 'Запустить по картинке',
 )
+if app_mode =='О приложении':
 
+    st.markdown('В этом приложении мы используем **MediaPipe** для создания маски лица. **Streamlit** для создания Веб графического пользовательского  Интерфейса')
+    st.markdown(
+    """
+    <style>
+    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
+        width: 400px;
+    }
+    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
+        width: 400px;
+        margin-left: -400px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+    )
+    st.markdown('''
+                Где может быть применим проект:
+                - Безопасность и идентификация
+                - Медицинская диагностика
+                - Образование
+                - Приложения дополненной реальности (AR)
+                ''')
+
+    st.markdown('''
+          # Обо мне \n 
+            Привет я  Коновалов Игорь  начинающий Data-Scientist. \n
+           
+            Если вы заинтересованы в построении  приложений компьютерного зрения(CV) как это вы можете написать мне, и мы можем обсудить вопрос дальнейшего сотрудничества. Мой телеграм: [@ColaChanel](https://t.me/ColaChannel)
+             \n
+            
+            Также можете связаться со мной:
+            - [LinkedIn](https://www.linkedin.com/in/igorkonovalovvasko/)
+            - [gmail](mailto:igor.konovalov.dev@gmail.com/)
+            - [Github](https://github.com/ColaChanel)
+        
+            
+             
+            ''')
+    # Если ты чувствуешь себя великодушным, то можешь купить мне  **чашку кофе** от [сюда](https://www.buymeacoffee.com/colachanel)
 # if app_mode =='About App':
 #     st.markdown('In this application we are using **MediaPipe** for creating a Face Mesh. **StreamLit** is to create the Web Graphical User Interface (GUI) ')
 #     st.markdown(
@@ -105,14 +145,14 @@ app_mode = st.sidebar.selectbox('Choose the App mode',
 #             If you are feeling generous you can buy me a **cup of  coffee ** from [HERE](https://augmentedstartups.info/ByMeACoffee)
              
 #             ''')
-if app_mode =='Run on Video':
+if app_mode =='Запустить по видео':
 
-    st.set_option('deprecation.showfileUploaderEncoding', False)
+    # st.set_option('deprecation.showfileUploaderEncoding', False)
 
-    use_webcam = st.sidebar.button('Use Webcam')
-    record = st.sidebar.checkbox("Record Video")
+    use_webcam = st.sidebar.button('Использовать камеру')
+    record = st.sidebar.checkbox("Записать видео")
     if record:
-        st.checkbox("Recording", value=True)
+        st.checkbox("Запись", value=True)
 
     # st.sidebar.markdown('---')
     # st.markdown(
@@ -130,17 +170,17 @@ if app_mode =='Run on Video':
     # unsafe_allow_html=True,
     #     )
     # max faces
-    max_faces = st.sidebar.number_input('Maximum Number of Faces', value=1, min_value=1)
+    max_faces = st.sidebar.number_input('Максимальное количество лиц', value=1, min_value=1)
     st.sidebar.markdown('---')
-    detection_confidence = st.sidebar.slider('Min Detection Confidence', min_value =0.0,max_value = 1.0,value = 0.5)
-    tracking_confidence = st.sidebar.slider('Min Tracking Confidence', min_value = 0.0,max_value = 1.0,value = 0.5)
+    detection_confidence = st.sidebar.slider('Минимальная точность детекции', min_value =0.0,max_value = 1.0,value = 0.5)
+    tracking_confidence = st.sidebar.slider('Минимальная уверенность при отслеживании', min_value = 0.0,max_value = 1.0,value = 0.5)
 
     st.sidebar.markdown('---')
 
-    st.markdown(' ## Output')
+    st.markdown(' ## Выход')
 
     stframe = st.empty()
-    video_file_buffer = st.sidebar.file_uploader("Upload a video", type=[ "mp4", "mov",'avi','asf', 'm4v' ])
+    video_file_buffer = st.sidebar.file_uploader("Загрузить видео", type=[ "mp4", "mov",'avi','asf', 'm4v' ])
     tfflie = tempfile.NamedTemporaryFile(delete=False)
 
 
@@ -151,9 +191,9 @@ if app_mode =='Run on Video':
             vid = cv2.VideoCapture(DEMO_VIDEO)
             tfflie.name = DEMO_VIDEO
     
-    # else:
-    #     tfflie.write(video_file_buffer.read())
-    #     vid = cv2.VideoCapture(tfflie.name)
+    else:
+        tfflie.write(video_file_buffer.read())
+        vid = cv2.VideoCapture(tfflie.name)
 
     width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -163,8 +203,8 @@ if app_mode =='Run on Video':
     codec = cv2.VideoWriter_fourcc('V','P','0','9')
     out = cv2.VideoWriter('output1.mp4', codec, fps_input, (width, height))
 
-    st.sidebar.text('Input Video')
-    st.sidebar.video(tfflie.name)
+    st.sidebar.text('Входящее Видео')
+    # st.sidebar.video(tfflie.name)
     fps = 0
     i = 0
     drawing_spec = mp_drawing.DrawingSpec(thickness=2, circle_radius=2)
@@ -172,15 +212,15 @@ if app_mode =='Run on Video':
     kpi1, kpi2, kpi3 = st.columns(3)
 
     with kpi1:
-        st.markdown("**FrameRate**")
+        st.markdown("**Частота кадров**")
         kpi1_text = st.markdown("0")
 
     with kpi2:
-        st.markdown("**Detected Faces**")
+        st.markdown("**Обнаруженны лица**")
         kpi2_text = st.markdown("0")
 
     with kpi3:
-        st.markdown("**Image Width**")
+        st.markdown("**размер изображения**")
         kpi3_text = st.markdown("0")
 
     st.markdown("<hr/>", unsafe_allow_html=True)
@@ -228,7 +268,7 @@ if app_mode =='Run on Video':
             frame = image_resize(image = frame, width = 640)
             stframe.image(frame,channels = 'BGR',use_column_width=True)
 
-    st.text('Video Processed')
+    st.text('Видео в обработке')
 
     output_video = open('output1.mp4','rb')
     out_bytes = output_video.read()
@@ -236,6 +276,137 @@ if app_mode =='Run on Video':
 
     vid.release()
     out. release()
+# if app_mode =='Run on Video':
+
+#     st.set_option('deprecation.showfileUploaderEncoding', False)
+
+#     use_webcam = st.sidebar.button('Use Webcam')
+#     record = st.sidebar.checkbox("Record Video")
+#     if record:
+#         st.checkbox("Recording", value=True)
+
+#     # st.sidebar.markdown('---')
+#     # st.markdown(
+#     # """
+#     # <style>
+#     # [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
+#     #     width: 400px;
+#     # }
+#     # [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
+#     #     width: 400px;
+#     #     margin-left: -400px;
+#     # }
+#     # </style>
+#     # """,
+#     # unsafe_allow_html=True,
+#     #     )
+#     # max faces
+#     max_faces = st.sidebar.number_input('Maximum Number of Faces', value=1, min_value=1)
+#     st.sidebar.markdown('---')
+#     detection_confidence = st.sidebar.slider('Min Detection Confidence', min_value =0.0,max_value = 1.0,value = 0.5)
+#     tracking_confidence = st.sidebar.slider('Min Tracking Confidence', min_value = 0.0,max_value = 1.0,value = 0.5)
+
+#     st.sidebar.markdown('---')
+
+#     st.markdown(' ## Output')
+
+#     stframe = st.empty()
+#     video_file_buffer = st.sidebar.file_uploader("Upload a video", type=[ "mp4", "mov",'avi','asf', 'm4v' ])
+#     tfflie = tempfile.NamedTemporaryFile(delete=False)
+
+
+#     if not video_file_buffer:
+#         if use_webcam:
+#             vid = cv2.VideoCapture(0)
+#         else:
+#             vid = cv2.VideoCapture(DEMO_VIDEO)
+#             tfflie.name = DEMO_VIDEO
+    
+#     # else:
+#     #     tfflie.write(video_file_buffer.read())
+#     #     vid = cv2.VideoCapture(tfflie.name)
+
+#     width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+#     height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+#     fps_input = int(vid.get(cv2.CAP_PROP_FPS))
+
+#     #codec = cv2.VideoWriter_fourcc(*FLAGS.output_format)
+#     codec = cv2.VideoWriter_fourcc('V','P','0','9')
+#     out = cv2.VideoWriter('output1.mp4', codec, fps_input, (width, height))
+
+#     st.sidebar.text('Input Video')
+#     st.sidebar.video(tfflie.name)
+#     fps = 0
+#     i = 0
+#     drawing_spec = mp_drawing.DrawingSpec(thickness=2, circle_radius=2)
+
+#     kpi1, kpi2, kpi3 = st.columns(3)
+
+#     with kpi1:
+#         st.markdown("**FrameRate**")
+#         kpi1_text = st.markdown("0")
+
+#     with kpi2:
+#         st.markdown("**Detected Faces**")
+#         kpi2_text = st.markdown("0")
+
+#     with kpi3:
+#         st.markdown("**Image Width**")
+#         kpi3_text = st.markdown("0")
+
+#     st.markdown("<hr/>", unsafe_allow_html=True)
+
+#     with mp_face_mesh.FaceMesh(
+#     min_detection_confidence=detection_confidence,
+#     min_tracking_confidence=tracking_confidence , 
+#     max_num_faces = max_faces) as face_mesh:
+#         prevTime = 0
+
+#         while vid.isOpened():
+#             i +=1
+#             ret, frame = vid.read()
+#             if not ret:
+#                 continue
+
+#             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#             results = face_mesh.process(frame)
+
+#             frame.flags.writeable = True
+#             frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+
+#             face_count = 0
+#             if results.multi_face_landmarks:
+#                 for face_landmarks in results.multi_face_landmarks:
+#                     face_count += 1
+#                     mp_drawing.draw_landmarks(
+#                     image = frame,
+#                     landmark_list=face_landmarks,
+#                     connections=mp_face_mesh.FACEMESH_TESSELATION,
+#                     landmark_drawing_spec=drawing_spec,
+#                     connection_drawing_spec=drawing_spec)
+#             currTime = time.time()
+#             fps = 1 / (currTime - prevTime)
+#             prevTime = currTime
+#             if record:
+#                 #st.checkbox("Recording", value=True)
+#                 out.write(frame)
+#             #Dashboard
+#             kpi1_text.write(f"<h1 style='text-align: center; color: red;'>{int(fps)}</h1>", unsafe_allow_html=True)
+#             kpi2_text.write(f"<h1 style='text-align: center; color: red;'>{face_count}</h1>", unsafe_allow_html=True)
+#             kpi3_text.write(f"<h1 style='text-align: center; color: red;'>{width}</h1>", unsafe_allow_html=True)
+
+#             frame = cv2.resize(frame,(0,0),fx = 0.8 , fy = 0.8)
+#             frame = image_resize(image = frame, width = 640)
+#             stframe.image(frame,channels = 'BGR',use_column_width=True)
+
+#     st.text('Video Processed')
+
+#     output_video = open('output1.mp4','rb')
+#     out_bytes = output_video.read()
+#     st.video(out_bytes)
+
+#     vid.release()
+#     out. release()
 
 # elif app_mode =='Run on Image':
 
