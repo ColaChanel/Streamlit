18de3b0ebbf3b02f0f93679cd3bd4862d43d73f2
ColaChanel <igorvaskon@gmail.com>
2024-09-14 19:48:12 +0300

    some changes of video

--- online/fresh_view/Demos/face_mesh_app.py
+++ online/fresh_view/Demos/face_mesh_app.py
@@ -39,34 +39,21 @@ st.sidebar.subheader('Параметры')
 
 @st.cache_data()
 def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
-    # initialize the dimensions of the image to be resized and
-    # grab the image size
     dim = None
     (h, w) = image.shape[:2]
 
-    # if both the width and height are None, then return the
-    # original image
     if width is None and height is None:
         return image
 
-    # check to see if the width is None
     if width is None:
-        # calculate the ratio of the height and construct the
-        # dimensions
         r = height / float(h)
         dim = (int(w * r), height)
-
-    # otherwise, the height is None
     else:
-        # calculate the ratio of the width and construct the
-        # dimensions
         r = width / float(w)
         dim = (width, int(h * r))
 
-    # resize the image
     resized = cv2.resize(image, dim, interpolation=inter)
 
-    # return the resized image
     return resized
 
 app_mode = st.sidebar.selectbox('Выберите вид взаимодействия',
@@ -189,7 +176,7 @@ if app_mode =='Запустить по видео':
 
     if not video_file_buffer:
         if use_webcam:
-            vid = cv2.VideoCapture(0)
+            vid = cv2.VideoCapture(0, cv2.CAP_DSHOW)
         else:
             vid = cv2.VideoCapture(DEMO_VIDEO)
             tfflie.name = DEMO_VIDEO
@@ -241,12 +228,8 @@ if app_mode =='Запустить по видео':
                 continue
 
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            # gpu
-            # gpu_frame = cv2.cuda_GpuMat()
-            # gpu_frame.upload(frame)
 
             results = face_mesh.process(frame)
-            # results = face_mesh.process(gpu_frame)
 
             frame.flags.writeable = True
             frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
@@ -265,7 +248,6 @@ if app_mode =='Запустить по видео':
             fps = 1 / (currTime - prevTime)
             prevTime = currTime
             if record:
-                #st.checkbox("Recording", value=True)
                 out.write(frame)
             #Dashboard
             kpi1_text.write(f"<h1 style='text-align: center; color: red;'>{int(fps)}</h1>", unsafe_allow_html=True)
